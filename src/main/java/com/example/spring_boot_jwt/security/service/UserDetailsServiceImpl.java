f4af8ff120ce20727c51839db7c02fea30d2498f
Ignacio Bardin <ilbardin@gmail.com>
2023-03-16 20:15:24 -0300

    Several code implementations. - Implement UserDetails. - Implement UserDetailsService. Add WebSecurityConfig.

---
+++ src/main/java/com/example/spring_boot_jwt/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.spring_boot_jwt.security.service;
+
+import com.example.spring_boot_jwt.model.User;
+import com.example.spring_boot_jwt.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with name: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
